386f066e6c47d55f60c0e3c1e01818dbc57590e9
strads <tomkjk@naver.com>
2025-01-13 03:48:48 +0900

    re

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: './frontend',  // frontend 폴더를 루트로 지정
   base: '/wordtalktalk_tts/',  // 리포지토리 이름을 base로 설정
   plugins: [react()],
 })
